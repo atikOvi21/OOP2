c86439075dd9b9265fc3c2272953c58e8e4f1930
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-09-14 10:23:46 +0600

    Fixed fizz test

--- SWE4302_OOP_2/Lab_06/FizzBuzz_/src/FizzBuzzTest.java
+++ SWE4302_OOP_2/Lab_06/FizzBuzz_/src/FizzBuzzTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FizzBuzzTest {
@@ -7,24 +8,32 @@ public class FizzBuzzTest {
     public void findFizzTest(){
         String expected = "Fizz";
         String result = fizzBuzz.getFizzyBuzz(3);
+
+        Assert.assertEquals(expected, result);
     }
 
     @Test
     public void findBuzzTest(){
         String expected = "Buzz";
         String result = fizzBuzz.getFizzyBuzz(7);
+
+        Assert.assertEquals(expected, result);
     }
 
     @Test
     public void findFizzBuzzTest(){
         String expected = "Fizzbuzz";
         String result = fizzBuzz.getFizzyBuzz(21);
+
+        Assert.assertEquals(expected, result);
     }
 
     @Test
     public void findGotchaTest(){
         String expected = "Gotcha";
         String result = fizzBuzz.getFizzyBuzz(32);
+
+        Assert.assertEquals(expected, result);
     }
 
 }
