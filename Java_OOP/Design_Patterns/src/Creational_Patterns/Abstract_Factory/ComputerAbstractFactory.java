feec643105b4fc96324effbb8338b6bd8dc666dc
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-10-16 21:08:59 +0600

    Added builder, prototype

---
+++ Java_OOP/Design_Patterns/src/Creational_Patterns/Abstract_Factory/ComputerAbstractFactory.java
@@ -0,0 +1,5 @@
+package Creational_Patterns.Abstract_Factory;
+
+public interface ComputerAbstractFactory {
+    public Computer createComputer();
+}
---
+++ Java_OOP/Design_Patterns/src/Creational_Patterns/Abstract_Factory/Main.java
@@ -0,0 +1,14 @@
+package Creational_Patterns.Abstract_Factory;
+
+public class Main {
+    public static void main(String[] args) {
+        testAbstractFactory();
+    }
+
+    private static void testAbstractFactory() {
+        Computer pc = ComputerFactory.getComputer(new PCFactory("64 GB","5 TB","4.9 GHz"));
+        Computer server = ComputerFactory.getComputer(new ServerFactory("16 GB","1 TB","2.9 GHz"));
+        System.out.println("AbstractFactory PC Config::"+pc);
+        System.out.println("AbstractFactory Server Config::"+server);
+    }
+}
