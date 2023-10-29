9069ea6d875603f83b6d3018d05db8a71d3c1840
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-10-29 14:56:03 +0600

    Added shuffle

--- Java_OOP/Collectionss/src/Shuffle/Main.java
+++ Java_OOP/Collectionss/src/Shuffle/Main.java
@@ -2,6 +2,7 @@ package Shuffle;
 
 public class Main {
     public static void main(String[] args) {
-
+        DeckOfCards myDeckOfCards = new DeckOfCards();
+        myDeckOfCards.printCards();
     }
 }
