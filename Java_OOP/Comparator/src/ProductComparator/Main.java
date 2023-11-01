17b72268b40432c648510d60a9e2c572367a339e
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-11-01 21:24:53 +0600

    Added Comparator and CustomComparators

---
+++ Java_OOP/Comparator/src/ProductComparator/Main.java
@@ -0,0 +1,25 @@
+package ProductComparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Keyboard", 20.0));
+        products.add(new Product(3, "Monitor", 100.0));
+        products.add(new Product(2, "Mouse", 10.0));
+        products.add(new Product(4, "Printer", 150.0));
+
+        for (Product product : products) {
+            System.out.println(product.getName() + " " + product.getPrice());
+        }
+
+        ProductComparator productComparator = new ProductComparator();
+        products.sort(productComparator);
+        System.out.println("After sorting:");
+        for (Product product : products){
+            System.out.println(product.getName() + " " + product.getPrice());
+        }
+    }
+}
