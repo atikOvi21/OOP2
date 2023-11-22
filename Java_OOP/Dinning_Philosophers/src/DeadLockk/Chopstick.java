504be7de70c633e5535b49d0750359857a47a7e5
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-11-22 13:10:47 +0600

    Added the DeadLock problematic solution as well

---
+++ Java_OOP/Dinning_Philosophers/src/DeadLockk/Chopstick.java
@@ -0,0 +1,18 @@
+package DeadLockk;
+
+import java.util.concurrent.Semaphore;
+
+public class Chopstick {
+    public Semaphore semaphore = new Semaphore(1);
+    void grabChopstick(){
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    void dropChopstick(){
+        semaphore.release();
+    }
+}
+
