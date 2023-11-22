5e5e84ec5aa928d2e2d9cdc1b4b708896b5a6f2b
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-11-22 13:03:34 +0600

    Implemented the Dinning problem with Odd_Even Solution

---
+++ Java_OOP/Dinning_Philosophers/src/Odd_Even/Chopstick.java
@@ -0,0 +1,18 @@
+package Odd_Even;
+
+import java.util.concurrent.Semaphore;
+
+public class Chopstick {
+    public Semaphore semaphore = new Semaphore(1);
+    void grabChopstick(){
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    void dropChopstick(){
+        semaphore.release();
+    }
+}
+
---
+++ Java_OOP/Dinning_Philosophers/src/Odd_Even/Philosopher.java
@@ -0,0 +1,40 @@
+package Odd_Even;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Philosopher implements Runnable{
+    private final int id;
+    private Chopstick leftChopstick;
+    private Chopstick rightChopstick;
+
+    public Philosopher(int id, Chopstick leftChopstick, Chopstick rightChopstick) {
+        this.id = id;
+        this.leftChopstick = leftChopstick;
+        this.rightChopstick = rightChopstick;
+    }
+
+    public void performAction(String action) {
+        int waitTime = ThreadLocalRandom.current().nextInt(0, 1000);
+        System.out.println(STR."Philosopher \{id}: \{action} for \{waitTime} milliseconds.");
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            performAction("Thinking");
+            leftChopstick.grabChopstick();
+            System.out.println(STR."Philosopher \{id}: Picked up left chopstick.");
+            rightChopstick.grabChopstick();
+            System.out.println(STR."Philosopher \{id}: Picked up right chopstick.");
+            performAction("Eating");
+            leftChopstick.dropChopstick();
+            System.out.println(STR."Philosopher \{id}: Dropped left chopstick.");
+            rightChopstick.dropChopstick();
+            System.out.println(STR."Philosopher \{id}: Dropped right chopstick.");
+            System.out.println(STR."Philosopher \{id}: Done eating.");
+            System.out.println(STR."Philosopher \{id}: Thinking again.");
+            System.out.println("---------------------------------------------");
+        }
+    }
+}
+
